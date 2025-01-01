fddeca3086ac94649e0100c1f5edfda764b12d40
Taewan Kim <paladyn12@naver.com>
2025-01-01 23:59:03 +0900

    init

---
+++ src/main/java/GachaCounter/GachaCounterApplication.java
@@ -0,0 +1,13 @@
+package GachaCounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachaCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GachaCounterApplication.class, args);
+	}
+
+}
